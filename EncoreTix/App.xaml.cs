1306b25bcb9901f100159da28fb9e255029dd5a7
papajeremy <jeremy@bryantbergfamily.com>
2024-09-30 16:42:10 -0700

    Add project files.

---
+++ EncoreTix/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EncoreTix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ EncoreTix/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EncoreTix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
